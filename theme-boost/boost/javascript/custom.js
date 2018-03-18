125f0453588b07be7bf334b73da2a72d96d1ffcf
chillinhen <c.hillen@gmx.net>
2018-03-18 10:59:58 +0100

    thumbs

--- theme-boost/boost/javascript/custom.js
+++ theme-boost/boost/javascript/custom.js
@@ -3,8 +3,14 @@ require(['jquery'], function ($) {
     var drawer = $('button[aria-controls="nav-drawer"]');
     var thumbParent = $('#region-main');
 
+    if (drawer.attr('aria-expanded') === 'false') {
+        thumbParent.addClass('larger');
+    } else {
+        thumbParent.removeClass('larger');
+    }
+
     drawer.click(function () {
-        thumbParent.toggleClass('smaller');
+        thumbParent.toggleClass('larger');
     });
 });
 
