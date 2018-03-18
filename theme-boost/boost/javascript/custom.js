5b24d308a35e8eb75a74ac46728776ff4bf7ea9a
chillinhen <c.hillen@gmx.net>
2018-03-18 15:31:34 +0100

    thumbnails

--- theme-boost/boost/javascript/custom.js
+++ theme-boost/boost/javascript/custom.js
@@ -1,5 +1,6 @@
 require(['jquery'], function ($) {
-
+//alert('cache löschen');
+    // Thumbnails
     var drawer = $('button[aria-controls="nav-drawer"]');
     var thumbParent = $('#region-main');
 
@@ -12,5 +13,13 @@ require(['jquery'], function ($) {
     drawer.click(function () {
         thumbParent.toggleClass('larger');
     });
+    //Order Thumbnails Content
+    var thumbnail = $('.coursebox > .content > .courseimage');
+    thumbnail.each(function () {
+        if ($(this).length > 0) {
+            //alert('treffer');
+            $(this).parent('div').parent('div').addClass('hasThumbnail');
+        }
+    });
+    
 });
-
