eac411f40cbd0c2127fad9a3122c89f521578ec6
chillinhen <c.hillen@gmx.net>
2018-09-20 08:04:22 +0200

    div

--- theme-boost/boost/javascript/snppet.js
+++ theme-boost/boost/javascript/snppet.js
@@ -1,10 +1,11 @@
 require(['jquery'], function ($) {
 
-
+$(".gradereport-grader-table tr").each(function(){
+   var label = $(this).children("th").children('span').html();
+   $(this).css('border','1px solid red');
+  console.log(label);
+});
 
 });
 
-
-
-▶
-◀
+//(FEHLER -> scrolling)
